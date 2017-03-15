cf36cc1d49386ccab48cc19ebd83283fbebe47cd
Lucas <Lucas@172.20.21.127>
2017-03-14 21:39:32 -0300

    Encapsulamento das operações de acesso aos contatos

---
+++ src/br/edu/impacta/ads/IContatoDao.java
@@ -0,0 +1,11 @@
+package br.edu.impacta.ads;
+import java.util.List;
+
+public interface IContatoDao {
+	
+	List<Contato>buscar(String nome);
+	void inserir(Contato c);
+	boolean exite(Contato c);
+	
+
+}
